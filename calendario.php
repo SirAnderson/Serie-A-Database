7446147396f5b672ce45526838d3f8ffad6838c9
SirAnderson <manuel.deleo@studenti.unipr.it>
2014-04-05 17:01:15 +0200

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -1,12 +1,14 @@
 <!-- 
      Copyright 2014 Manuel Deleo, Lorenzo Livrini 
+     
+     This file is part of Serie A Database.
 
-     SerieADatabase is free software: you can redistribute it and/or modify
+     Serie A Database is free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
      the Free Software Foundation, either version 3 of the License, or
      (at your option) any later version.
 
-     SerieADatabase is distributed in the hope that it will be useful,
+     Serie A Database is distributed in the hope that it will be useful,
      but WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
      GNU General Public License for more details.
